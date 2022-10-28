43c979efd86b2106372d99bcfe465385ea0994d0
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2022-10-28 14:01:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function Elküld() {
+    document.getElementById("visszaNev").innerHTML = document.getElementById("nev").value;
+    document.getElementById("visszaCim").innerHTML = document.getElementById("cim").value;
+    document.getElementById("visszaTel").innerHTML = document.getElementById("tel").value;
+}
